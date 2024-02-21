0fe9f5b69e089142d091b4ed2f4a314e8c532a4b
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-02-21 06:28:08 -0600

    Update 169-MajorityElement.cpp

--- 169-MajorityElement.cpp
+++ 169-MajorityElement.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-        int majority(0);
-        int count(0);
+        int x(0), cnt(0);
         for(int p = 0; p < nums.size(); p++){
-            if(count <= 0){
-                count = 1;
-                majority = nums[p];
-            }
-            else{
-                if(nums[p] == majority){++count;}
-                else{--count;}
-            }
+            if(cnt <= 0){x = nums[p]; cnt = 1;}
+            else if(nums[p] == x){++cnt;}
+            else if(nums[p] != x){--cnt;}
         }
 
-        return majority;
-        
+        return x;
     }
-};
+}
