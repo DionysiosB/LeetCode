4c02f01c0eb294ba7a50656ec61a5dd81cea4026
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-12-03 09:08:18 -0600

    Create 2176-CountEqualAndDivisiblePairsInAnArray.cpp

---
+++ 2176-CountEqualAndDivisiblePairsInAnArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int cnt(0);
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if((i * j) % k){continue;}
+                if(nums[i] != nums[j]){continue;}
+                ++cnt;
+            }
+        }
+
+        return cnt;
+    }
+};
