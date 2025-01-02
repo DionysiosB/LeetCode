727fe1965811cf3f869996ec951dfc44149e5fb2
Dionysios Barmpoutis <barmpoutis@gmail.com>
2025-01-02 15:41:53 +0200

    Create 704-BinarySearch.cpp

---
+++ 704-BinarySearch.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left(0), right(nums.size() - 1);
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target){return mid;}
+            else if(nums[mid] < target){left = mid + 1;}
+            else if(nums[mid] > target){right = mid - 1;}
+        }
+
+        return -1;
+    }
+};
