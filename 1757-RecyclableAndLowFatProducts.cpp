f70604bea65f1df3aa8b20c87dea89159eebf9b1
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-05-28 07:36:10 -0500

    Create 1757-RecyclableAndLowFatProducts.cpp

---
+++ 1757-RecyclableAndLowFatProducts.cpp
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products[(products["low_fats"] == 'Y') & (products["recyclable"] == 'Y')].drop(columns=["low_fats", "recyclable"])
+    
