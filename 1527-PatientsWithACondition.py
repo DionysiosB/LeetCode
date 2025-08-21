bbae6507b4c741a08f2c5b582f5e2c097d79b27f
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-08-21 06:52:12 -0500

    Create 1527-PatientsWithACondition.py

---
+++ 1527-PatientsWithACondition.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    df = patients[patients['conditions'].str.contains(r'\bDIAB1')]
+    return df[['patient_id', 'patient_name', 'conditions']]
