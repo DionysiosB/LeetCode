d091ccf9eeb75ea84d4e99a9e2228ea2cc13e532
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-10-06 07:35:22 -0500

    Create 530-MinimumAbsoluteDifferenceInBST.cpp

---
+++ 530-MinimumAbsoluteDifferenceInBST.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void dfs(TreeNode* node, std::vector<int> &v){
+        if(!node){return;}
+        v.push_back(node->val);
+        dfs(node->left, v);
+        dfs(node->right, v);
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        std::vector<int> v;
+        dfs(root, v);
+        sort(v.begin(), v.end());
+        int mn(v.back());
+        for(int p = 1; p < v.size(); p++){
+            int diff = v[p] - v[p - 1];
+            mn = (mn < diff ? mn : diff);
+        }
+        return mn;
+    }
+};
