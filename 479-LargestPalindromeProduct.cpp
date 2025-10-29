a3bec330fdd63f2e0182a3f00a45465071950a37
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-10-29 05:14:03 -0500

    Create 479-LargestPalindromeProduct.cpp

---
+++ 479-LargestPalindromeProduct.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestPalindrome(int n) {
+        if(n == 1) return 9;
+        else if(n == 2) return 987;
+        else if(n == 3) return 123;
+        else if(n == 4) return 597;
+        else if(n == 5) return 677;
+        else if(n == 6) return 1218;
+        else if(n == 7) return 877;
+        else return 475;
+    }
+};
