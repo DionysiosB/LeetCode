af6fc3fb1e0664f9eae85d99fda585e1b478b9e7
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-02-21 17:26:19 -0500

    Create 33-SearchInRotatedSortedArray.cpp

---
+++ 33-SearchInRotatedSortedArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+ public:
+  int search(vector<int>& nums, int target) {
+    int l = 0;
+    int r = nums.size() - 1;
+
+    while (l <= r) {
+    int m = (l + r) / 2;
+      if (nums[m] == target){return m;}
+      if (nums[l] <= nums[m]){
+        if (nums[l] <= target && target < nums[m]){r = m - 1;}
+        else{l = m + 1;}
+      }
+      else{
+        if(nums[m] < target && target <= nums[r]){l = m + 1;}
+        else{r = m - 1;}
+      }
+    }
+
+    return -1;
+  };
+};
