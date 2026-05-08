b568323b6dd6f17d353c2ab24deda90ae3439e26
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-05-08 17:26:24 -0400

    Create 559-MaximumDepthOfNaryTree.cpp

---
+++ 559-MaximumDepthOfNaryTree.cpp
@@ -0,0 +1,30 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        
+        if(!root){return 0;}
+        int d(0);
+        for(Node* child : root->children){d = std::max(d, maxDepth(child));}
+        return 1 + d;
+    }
+};
