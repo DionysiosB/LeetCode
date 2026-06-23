1716e624d1ddfc5bb94e9e913426aa673f7d0e59
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-06-22 20:04:05 -0400

    Create 3263-ConvertDoublyLinkedListToArrayI.cpp

---
+++ 3263-ConvertDoublyLinkedListToArrayI.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for doubly-linked list.
+ * class Node {
+ *     int val;
+ *     Node* prev;
+ *     Node* next;
+ *     Node() : val(0), next(nullptr), prev(nullptr) {}
+ *     Node(int x) : val(x), next(nullptr), prev(nullptr) {}
+ *     Node(int x, Node *prev, Node *next) : val(x), next(next), prev(prev) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> toArray(Node *head){
+        std::vector<int> v;
+        while(head){v.push_back(head->val); head = head->next;}
+        return v;
+    }
+};
