8b562d0e8f5ffa0193d108fa8d189289d37a29ae
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-09-11 19:08:03 -0400

    Create 780-ReachingPoints.cpp

---
+++ 780-ReachingPoints.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+
+        while(tx >= sx && ty >= sy){
+            if(tx == ty){break;}
+            else if(tx > ty){
+                if(ty > sy){tx %= ty;}
+                else{return (tx - sx) % ty == 0;}
+            }
+            else{
+                if(tx > sx){ty %= tx;}
+                else{return (ty - sy) % tx == 0;}
+            }
+        }
+
+        return (tx == sx && ty == sy);
+        
+    }
+};
